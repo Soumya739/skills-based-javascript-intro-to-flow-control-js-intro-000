cc332869472e1e535aa22f83b6a7c4e5fb3de6fa
Soumya Goyal <somyagoyal739@gmail.com>
2019-08-03 07:37:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -27,7 +27,7 @@ function ternaryTeenager(age) {
   if (age >= 13 && age <19){
     return  "You are a teenager";
   }
-    return  "You are not a teenager!";
+    return  "You are not a teenager";
 }
 
 function switchAge(age) {
